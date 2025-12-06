6145ac9727b4be79854a9941833d649dbfa7c255
techrabbit58 <dzb@pruefbit.de>
2025-12-06 10:15:36 +0100

    day 6 part 1 cleaner code and now with two alternative solutions

--- src/day06.py
+++ src/day06.py
@@ -2,7 +2,9 @@
 AdventOfCode 2025 Day 6
 https://adventofcode.com/2025/day/6
 """
+import operator
 import time
+from functools import reduce
 from pathlib import Path
 
 example = """123 328  51 64 
@@ -28,15 +30,31 @@ def parse(text: str) -> ProblemList:
     return problems
 
 
-def solve_part1(puzzle_input: ProblemList) -> int:
+def solve_part1_a(puzzle: ProblemList) -> int:
+    """
+    WARNING! This is slower, and using eval() on untrustful input
+    is dangerous. The eval() function takes its input as python
+    source code and then executes it. This can do anything that _you_
+    can do on your computer!
+    """
+    return sum(eval(problem[-1].join(problem[:-1])) for problem in puzzle)
+
+
+def solve_part1(puzzle: ProblemList) -> int:
+    """
+    This is a safer solution than slove_part1_a(), and pretty way faster.
+    It first translates the
+    """
     answer = 0
-    for problem in puzzle_input:
-        answer += eval(problem[-1].join(problem[:-1]))
+    for problem in puzzle:
+        func, init = {"+": (operator.add, 0), "*": (operator.mul, 1)}[problem[-1]]
+        result = reduce(func, map(int, problem[:-1]), init)
+        answer += result
     return answer
 
 
-def solve_part2(puzzle_input: ProblemList) -> int:
-    return -1
+def solve_part2(puzzle: ProblemList) -> int:
+    ...
 
 
 def main() -> None:
