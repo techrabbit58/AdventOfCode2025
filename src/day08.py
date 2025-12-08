ccf3616058d5285eaf5ed83b675cfda1bb3ebbf1
techrabbit58 <dzb@pruefbit.de>
2025-12-08 16:04:00 +0100

    day 8 code shaping

--- src/day08.py
+++ src/day08.py
@@ -30,7 +30,8 @@ example = """162,817,812
 425,690,689""", 40, 25272
 
 type Vector = tuple[int, int, int]  # v = (x, y, z)
-type Circuit = set[Vector]
+type NodeId = int
+type Circuit = set[NodeId]
 
 
 def parse(text: str) -> set[Vector]:
@@ -44,71 +45,81 @@ def squared_distance(p: Vector, q: Vector) -> int:
     return abs(p[0] - q[0]) ** 2 + abs(p[1] - q[1]) ** 2 + abs(p[2] - q[2]) ** 2
 
 
-def solve(vectors: set[Vector], limit: int = None) -> int:
-    nodes = {
-        squared_distance(p, q): {p, q}
-        for p, q in combinations(vectors, 2)
+def solve(vectors: set[Vector], limit: int | None = None) -> int:
+    """
+    This solves part 1 if the limit is set.
+    This solves part 2 if the limit is not set.
+    """
+    nodes = {i: j for i, j in enumerate(vectors)}
+    connection_by_cost = {
+        squared_distance(p, q): {i, j}
+        for (i, p), (j, q) in combinations(nodes.items(), 2)
     }
+    ranks = sorted(connection_by_cost.keys())
 
-    connection_cost = sorted(nodes.keys())
     circuits = deque()
-    circuits.append(nodes[connection_cost[0]])
+    circuits.append(connection_by_cost[ranks[0]])
     last_seen = list(circuits[0])
-    singles = vectors.copy()
+    singles = set(nodes.keys())
 
-    for i, cost in enumerate(connection_cost[1:], 1):
+    for i, current_rank in enumerate(ranks[1:], 1):
         if i == limit or not singles:  # limit: part 1, singles: part 2
             break
 
-        connection = nodes[cost]
-        singles -= connection  # part 2
+        connection = connection_by_cost[current_rank]
+        singles -= connection
         last_seen = list(connection)  # part 2
 
-        # step 1: add new connection to existing circuit or add as new subcircuit
-        for circuit in circuits:
-            if circuit & connection:
-                circuit |= connection
-                break
-        else:
-            circuits.append(connection)
-
-        # step 2: connect subcircuits
-        for c1, c2 in combinations(circuits, 2):
-            if c1 & c2:
-                connected_circuit = c1 | c2
-                circuits.remove(c1)
-                circuits.remove(c2)
-                circuits.append(connected_circuit)
+        connect(circuits, connection)
+        merge(circuits)
 
     # part 1:
     # add vectors that are still unconnected
-    # just to be sure to catch all circuits for part 1,
-    # due to a remark in the explanation of AoCs part 1 example
+    # just to be sure to catch all circuits for part 1
+    # (this is due to a remark in the explanation of AoCs part 1 example)
     for single in singles:
         circuits.append({single})
 
-    circuit_sizes = sorted([len(circuit) for circuit in circuits], reverse=True)
+    circuit_sizes = sorted([len(circuit) for circuit in circuits], reverse=True)  # part 1
+    part1 = math.prod(circuit_sizes[:3])
 
-    if limit is None:  # part 2
-        (x1, _, _), (x2, _, _) = last_seen
-        answer = x1 * x2
-    else:  # part 1
-        answer = math.prod(circuit_sizes[:3])
+    n1, n2 = last_seen  # part 2
+    part2 = nodes[n1][0] * nodes[n2][0]
 
-    return answer
+    return part1 if limit is not None else part2
+
+
+def merge(circuits: deque[Circuit]) -> None:
+    for c1, c2 in combinations(circuits, 2):
+        if c1 & c2:
+            connected_circuit = c1 | c2
+            circuits.remove(c1)
+            circuits.remove(c2)
+            circuits.append(connected_circuit)
+
+
+def connect(circuits: deque[Circuit], new_pair: Circuit) -> None:
+    for circuit in circuits:
+        if circuit & new_pair:
+            circuit |= new_pair
+            break
+    else:
+        circuits.append(new_pair)
 
 
 def main() -> None:
     ex_vectors = parse(example[0])
+    ex_limit = 10
 
-    if solve(ex_vectors, 10) != example[1]:
+    if solve(ex_vectors, ex_limit) != example[1]:
         print("Part 1 not done")
         exit()
 
     vectors = parse(Path(__file__).with_suffix(".txt").read_text())
+    limit = 1000
 
     start = time.perf_counter()
-    answer = solve(vectors, len(vectors))
+    answer = solve(vectors, limit)
     end = time.perf_counter()
     print(f"Part 1 solution: {answer}, runtime = {end - start:.3f} s")
 
